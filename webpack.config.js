f92a11f1756db49dd13b96a34e75eeb5367bdb68
Elizabeth Harper <epharper@upenn.edu>
2020-02-20 21:03:45 -0500

    i swear to Belial i'm gonna slap the crap out of you, travis

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        exclude: /node_modules|dist|out/,
+        exclude: /node_modules|dist\/|out\//,
         use: 'ts-loader'
       },
       {
