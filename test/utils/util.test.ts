1d0efceaba247c7a26b4dfa74542d850a91b6490
Elizabeth Harper <epharper@upenn.edu>
2020-02-26 23:54:58 -0500

    18.05% coverage

--- test/utils/util.test.ts
+++ test/utils/util.test.ts
@@ -1,6 +1,6 @@
 ///<reference path="../../node_modules/@types/jest/index.d.ts"/>
 
-import { arrayOmit, arrayUniq } from "../../src/lib/util";
+import { arrayOmit, arrayUniq, exists, lossyClone } from "../../src/lib/util";
 
 test("1", () => {
   const i = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -12,4 +12,18 @@ test("2", () => {
   const i = ["a", "b", "c", "a", "c", "d", "b", "d", "e"];
   const o = ["a", "b", "c", "d", "e"];
   expect(arrayUniq(i)).toStrictEqual(o);
+});
+
+test("3", () => {
+  expect(exists(null)).toStrictEqual(false);
+  expect(exists(undefined)).toStrictEqual(false);
+  expect(exists({})).toStrictEqual(true);
+});
+
+test("4", () => {
+  const i = {a: {b: {c: {d: "e"}}, f: "g"}, h: "i"};
+  const t = lossyClone(i);
+  expect(t).toStrictEqual(i);
+  i.h = "apples";
+  expect(t).not.toStrictEqual(i);
 });
